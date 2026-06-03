50a74651cb586329a39fe56b281539a0b582b166
Lisa <47106816+lisa0510@users.noreply.github.com>
2026-06-03 20:54:25 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ Promise.all([
 const config = {
   type: Phaser.AUTO,
   parent: "app",
-  scene: [Shop,Ending],
+  scene: [Menu, Intro, Tutorial, Shop, Ending],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
